5310edc9a7511881e7cabc07047fe324a2ce8979
._______166 <retromac9x2@gmail.com>
2026-09-11 22:49:27 +0100

    add mozc candidate provider
    needs k3lp subtypes to be implemented for me to test it

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/nlp/NlpManager.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/nlp/NlpManager.kt
@@ -29,6 +29,7 @@ import dev.patrickgold.florisboard.ime.editor.EditorContent
 import dev.patrickgold.florisboard.ime.editor.EditorRange
 import dev.patrickgold.florisboard.ime.media.emoji.EmojiSuggestionProvider
 import dev.patrickgold.florisboard.ime.nlp.latin.LatinLanguageProvider
+import dev.patrickgold.florisboard.ime.nlp.mozc.MozcCandidateProvider
 import dev.patrickgold.florisboard.keyboardManager
 import dev.patrickgold.florisboard.lib.util.NetworkUtils
 import dev.patrickgold.florisboard.subtypeManager
@@ -65,6 +66,7 @@ class NlpManager(context: Context) {
         mapOf(
             LatinLanguageProvider.ProviderId to ProviderInstanceWrapper(LatinLanguageProvider(context)),
             //HanShapeBasedLanguageProvider.ProviderId to ProviderInstanceWrapper(HanShapeBasedLanguageProvider(context)),
+            MozcCandidateProvider.ProviderId to ProviderInstanceWrapper(MozcCandidateProvider())
         )
     }
     // lock unnecessary because values constant
