53da7513397dfcd69961d8a04fc26adcdcbfc50a
._______166 <retromac9x2@gmail.com>
2026-09-19 16:43:36 +0100

    pass left and right arrow keys into mozc

--- app/src/main/kotlin/dev/patrickgold/florisboard/ime/keyboard3/ImeController.kt
+++ app/src/main/kotlin/dev/patrickgold/florisboard/ime/keyboard3/ImeController.kt
@@ -415,10 +415,10 @@ class ImeController(
             val windowController = FlorisImeService.windowControllerOrNull()
             when (descriptor) {
                 // TODO evaluate use of modern cursor anchor API instead of sending raw key events
-                ImeActions.ArrowDown -> state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_DOWN)
-                ImeActions.ArrowLeft -> state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_LEFT)
-                ImeActions.ArrowRight -> state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_RIGHT)
-                ImeActions.ArrowUp -> state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_UP)
+                ImeActions.ArrowDown -> sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_DOWN)
+                ImeActions.ArrowLeft -> sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_LEFT)
+                ImeActions.ArrowRight -> sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_RIGHT)
+                ImeActions.ArrowUp -> sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_UP)
                 ImeActions.Delete -> emitForwardDelete()
                 ImeActions.Settings -> FlorisImeService.launchSettings()
                 ImeActions.ShowTextPanel -> {
@@ -464,6 +464,26 @@ class ImeController(
                 ImeActions.CompactLayoutToRight -> windowController?.actions?.compactLayoutToRight()
                 ImeActions.ExternalVoiceInput -> FlorisImeService.switchToVoiceInputMethod()
                 else -> super.emitDescriptor(descriptor)
+            }
+        }
+
+        fun sendDownUpKeyEvent(keycode: Int) {
+            when (keycode) {
+                KeyEvent.KEYCODE_DPAD_LEFT -> {
+                    if (MozcEngine.instance.preedit.value.isNotEmpty()) {
+                        MozcEngine.instance.sendKey(ProtoCommands.KeyEvent.SpecialKey.LEFT)
+                    } else {
+                        state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_LEFT)
+                    }
+                }
+                KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                    if (MozcEngine.instance.preedit.value.isNotEmpty()) {
+                        MozcEngine.instance.sendKey(ProtoCommands.KeyEvent.SpecialKey.RIGHT)
+                    } else {
+                        state.editor.sendDownUpKeyEvent(KeyEvent.KEYCODE_DPAD_RIGHT)
+                    }
+                }
+                else -> state.editor.sendDownUpKeyEvent(keycode)
             }
         }
 
